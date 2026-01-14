074b3634009ce67b117372d600aa928f4f18464f
ganzlars <ganzhorn.lars@gmail.com>
2026-01-14 08:06:01 +0100

    Update ball_tree.py

--- ball_tree.py
+++ ball_tree.py
@@ -1,156 +1,123 @@
-"""
-def norm(x):
-    norm_squared = 0
-
-    try:
-        for component in x:
-            try:
-                norm_squared += component**2
-            except TypeError:
-                print("Expected float valued entries.")
-                sys.exit(1)
-    except AttributeError:
-        print("Expected list as input.")
-        sys.exit(1)
-
-    return math.sqrt(norm_squared)
-"""
-
 import math
-import random
 
-def norm(X):
-    return math.sqrt(sum(x**2 for x in X))
-
-def distance(X, Y):
+def distance(X, Y): #euklidische Distanz für zwei Punkte berechnen
     return math.sqrt(sum((x - y)**2 for x, y in zip(X, Y)))
 
-def farthest_point(X, set):
-    return max(set, key=lambda Y: distance(X, Y))
-
-def vector_sum(vectors):
-    return [sum(components) for components in zip(*vectors)]
-
-def sign(x):
-    return 1 if x >= 0 else -1
-
-
 class BallTree:
-    def __init__(self, data, leaf_size=1):
+    def __init__(self, data, leaf_size=10):
         self.leaf_size = leaf_size
-        self.data = data 
-        self.left = None
-        self.right = None
+        self.nodes = [] # Liste zur Speicherung der Baumstruktur
+        # Speicherstruktur eines Knotens: (center, radius, left_idx, right_idx, points)
+        self.root_idx = self._build_iterative(data)
 
-        self.center, self.radius = self._compute_center_radius()
+    def _build_iterative(self, data):   # erstellt Ball-Tree iterativ mittels eines Stacks
+        if not data:
+            return None
 
-        if len(data) > self.leaf_size:
-            self._split()
-    
-    def _compute_center_radius(self):
-        points = [entry[1] for entry in self.data]
-        center = [coord / len(points) for coord in vector_sum(points)]
-        return center, max(distance(point, center) for point in points)
-    
-    def _split(self):
-        points = [entry[1] for entry in self.data]
-        p1 = farthest_point(points[0], points)
-        p2 = farthest_point(p1, points) 
+        # Liste aller Knoten
+        self.nodes = []
 
-        left_data = []
-        right_data = []
+        root_placeholder_idx = 0
+        # Stack speichert Daten aus: Datenmenge, Ziel-Index in Knotenliste
+        stack = [(data, root_placeholder_idx)]
 
-        for d in self.data: 
-            if distance(d[1], p1) <= distance(d[1], p2):
-                left_data.append(d)
+        # Platzhalter für Wurzelknoten erstellen
+        self.nodes.append({})
+
+        while stack:
+            current_data, node_idx = stack.pop()
+
+            # Zentrum und Radius berechnen
+            points_coords = [p[1] for p in current_data]
+            center = self._compute_center(points_coords)
+            radius = max(distance(p[1], center) for p in current_data)
+
+            # Abbruchbedingung: Wenn Datenmenge klein genug --> Blattknoten erstellen
+            if len(current_data) <= self.leaf_size:
+                self.nodes[node_idx] = {
+                    'center': center,
+                    'radius': radius,
+                    'points': current_data,  # Enthält (label, coords)
+                    'left': None,
+                    'right': None
+                }
             else:
-                right_data.append(d)
+                # Splitting-Heuristik: Finde zwei weit entfernte Pole (p1, p2)
+                p1 = max(current_data, key=lambda p: distance(p[1], center))
+                p2 = max(current_data, key=lambda p: distance(p[1], p1[1]))
+
+                # Punkte dem näheren Pol zuordnen
+                left_data, right_data = [], []
+                for p in current_data:
+                    if distance(p[1], p1[1]) < distance(p[1], p2[1]):
+                        left_data.append(p)
+                    else:
+                        right_data.append(p)
+
+                # Indizes für Kinderknoten am Listenende reservieren
+                left_idx = len(self.nodes)
+                right_idx = len(self.nodes) + 1
+                self.nodes.append({})  # für left
+                self.nodes.append({})  # für right
+
+                # Aktuellen Knoten als inneren Knoten (Verzweigung) speichern
+                self.nodes[node_idx] = {
+                    'center': center,
+                    'radius': radius,
+                    'points': None,
+                    'left': left_idx,
+                    'right': right_idx
+                }
+
+                # Kinder auf Stack legen
+                stack.append((right_data, right_idx))
+                stack.append((left_data, left_idx))
+
+        return root_placeholder_idx
+
+    def _compute_center(self, points):
+        # Mittelpunkt der Punktemenge berechnen
+        d = len(points[0])
+        n = len(points)
+        return [sum(p[i] for p in points) / n for i in range(d)]
+
+    def query(self, target, k):
+        # Sucht k nächsten Nachbarn mittels Pruning
+        if self.root_idx is None: return []
         
-        self.left = BallTree(left_data, self.leaf_size)
-        self.right = BallTree(right_data, self.leaf_size)
-
-        self.data = None  # free memory
-
-    def knn_update(self, x, k, heap=[]): 
-        lower_bound = max(0.0, distance(x, self.center) - self.radius)
-
-        if len(heap) == k and lower_bound > heap[0][0]:
-            return 
+        neighbors = [] # Liste von (distanz, label)
+        stack = [self.root_idx]
         
-        if self.left is None and self.right is None: 
-            for d in self.data:
-                dist = distance(x, d[1])
-                if len(heap) < k-1:
-                    heap.append((dist, d))
-                elif len(heap) == k-1:
-                    heap.append((dist, d)) 
-                    heap.sort(reverse=True)
-                elif dist < heap[0][0]:
-                    heap[0] = (dist, d)
-                    heap.sort(reverse=True)
-            return 
-        
-        if distance(x, self.left.center) < distance(x, self.right.center):
-            self.left.knn_update(x, k, heap)
-            self.right.knn_update(x, k, heap)
-        else:
-            self.right.knn_update(x, k, heap)
-            self.left.knn_update(x, k, heap)
+        while stack:
+            idx = stack.pop()
+            node = self.nodes[idx]
+            
+            dist_to_center = distance(target, node['center'])
+            
+            # Pruning: Wenn Zielpunkt zu weit von Kugel weg, können darin keine besseren Nachbarn mehr liegen
+            if len(neighbors) == k and dist_to_center - node['radius'] >= max(neighbors)[0]:
+                continue
+                
+            if node['points'] is not None: # Blattknoten
+                for label, coords in node['points']:
+                    d = distance(target, coords)
+                    # Erhalte Top-K der bisher nächsten Punkte
+                    if len(neighbors) < k:
+                        neighbors.append((d, label))
+                        neighbors.sort()
+                    elif d < neighbors[-1][0]:
+                        neighbors[-1] = (d, label)
+                        neighbors.sort()
+            else: # Innerer Knoten
+                left = self.nodes[node['left']]
+                right = self.nodes[node['right']]
 
-    def knn_query(self, x, k):
-        heap = []
-        self.knn_update(x, k, heap)
-        return [p for (_, p) in heap]
-    
-    def predict(self, x, k):
-        knn = self.knn_query(x, k)
-        return sign(sum(labels for labels, _ in knn))
-    
-
-class Classifier:
-    def __init__(self, data, l, mode, K, leaf_size=1):
-        self.folds = self._generate_folds(data, l, mode)
-        self.folds_gapped = self._generate_folds_gapped() 
-        self.l = l 
-        self.K = K
-        self.leaf_size = leaf_size
-
-        self.trees = [BallTree(fold, leaf_size=self.leaf_size) for fold in self.folds_gapped]
-        self.k_opt = self._find_k_opt()
-
-
-    def _generate_folds(data, l, mode):
-        n = len(data)
-        folds = [[] for _ in range(l)]
-
-        if mode == 0:
-            random.shuffle(data) 
-            for i, item in enumerate(data):
-                folds[i % l].append(item)
-        else:
-            for idx, item in enumerate(data):
-                folds[idx % l].append(item)
-
-        return folds
-
-    def _generate_folds_gapped(self):
-        folds_gapped = []
-
-        for i in range(len(self.folds)):
-            gapped_fold = []
-            for j in range(len(self.folds)):
-                if j != i:
-                    gapped_fold.extend(self.folds[j])
-            folds_gapped.append(gapped_fold)
-
-        return folds_gapped
-
-    def _find_k_opt(self):
-        residues = []
-        for k in self.K:
-            avg_residue = sum(sum(abs(y - self.trees[i].predict(x, k)) for y, x in self.folds[i]) / len(self.folds[i]) for i in range(self.l))
-            residues.append((k, avg_residue))
-        return min(residues, key=lambda item: item[1])[0]
-
-    def predict(self, x):
-        return sign(sum(self.trees[i].predict(x, self.k_opt) for i in range(self.l)))
+                # Heuristik: Kugel zuerst besuchen, deren Zentrum näher liegt
+                if distance(target, left['center']) < distance(target, right['center']):
+                    stack.append(node['right'])
+                    stack.append(node['left'])
+                else:
+                    stack.append(node['left'])
+                    stack.append(node['right'])
+                    
+        return [n[1] for n in neighbors]
